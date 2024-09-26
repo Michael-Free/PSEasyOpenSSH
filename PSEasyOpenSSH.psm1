2b1991d73d038394c2e25ac83e01fddbcb2c5b34
Michael Free <michael.d.free@gmail.com>
2024-09-26 10:29:51 -0400

    Created PSD file and started install module

---
+++ PSEasyOpenSSH.psm1
@@ -0,0 +1,44 @@
+function Install-OpenSSHServer() {
+  $openSSHFeature = Get-WindowsCapability -Online | Where-Object { $_.Name -like 'OpenSSH.Server*' }
+
+  if ($openSSHFeature.State -eq 'Installed') {
+    throw 'OpenSSH Server already installed...'
+  }
+        
+  try {
+    Add-WindowsCapability -Online -Name $openSSHFeature.Name
+  }
+  catch {
+    throw 'Unable to install OpenSSH Server'
+  }
+
+  $serviceName = 'sshd'
+  $serviceStatus = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+
+  if (-not $serviceStatus) {
+    throw "$serviceName cannot be found."
+  }
+  elseif ($serviceStatus.Status -ne 'Running') {
+    try {
+      Start-Service -Name $serviceName
+    }
+    catch {
+      throw "Unable to start $serviceName service"
+    }
+  }
+}
+
+function Remove-OpenSSHServer() {
+}
+
+function Disable-OpenSSHServer() {
+}
+
+function Enable-OpenSSHServer() {
+}
+
+function Start-OpenSSHServer() {
+}
+
+function Stop-OpenSSHServer() {
+}
